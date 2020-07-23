1158aafdb0b954960477f742b79fe09fc9a5fbb6
Tien Tran <tientran2290@gmail.com>
2020-07-23 12:03:44 -0700

    1st commit, testing

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const MainScreen: React.FC = () => {
-  return <Text>Hello world</Text>;
+  return <Text>Hello world mainscreenTEST2</Text>;
 };
 
 export default MainScreen;
