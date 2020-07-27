0dd85b1c8544b041cd0a79a160d13da2fa399d63
Tien Tran <tientran2290@gmail.com>
2020-07-27 15:42:23 -0700

    MainScreen and Navigation

--- src/screens/PlateInventoryScreen.tsx
+++ src/screens/PlateInventoryScreen.tsx
@@ -1,9 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 const PlateIventoryScreen = () => {
-  return <Text>This is the main screen.</Text>;
+  return <Text>This is the PLATEINVENTORYSCREEN screen.</Text>;
 };
 
 export default PlateIventoryScreen;
