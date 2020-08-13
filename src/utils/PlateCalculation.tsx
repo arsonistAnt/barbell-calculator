c8932b3f353432f1ba57a90288cfd156085858d2
Anthony Nguyen <ant@Anthonys-MacBook-Pro.local>
2020-08-13 15:28:55 -0700

    Split plate calculation into two different methods
    - Two calculation methods added to the PlateCalculation.tsx module.
    - The 'limited' method takes into account the amount of plates available in the user settings.
    - The 'standard' method just assumes the user has an unlimited amount of plate available.

--- src/utils/PlateCalculation.tsx
+++ src/utils/PlateCalculation.tsx
@@ -109,7 +109,7 @@ export class DefaultPlateConfig implements PlateConfig {
   constructor(config?: DefaultPlateConfig) {
     if (config !== undefined) {
       this.barbellWeight = config.barbellWeight;
-      this.standardPlates = StandardPlate.createStandardLB();
+      this.standardPlates = config.standardPlates.slice();
       this.selectedPlates = new Set(config.selectedPlates);
       this.conversionType = config.conversionType;
       this.useLimitedPlates = config.useLimitedPlates;
@@ -142,59 +142,77 @@ export class DefaultPlateConfig implements PlateConfig {
 /**
  * Calculates the list of plates needed to hit the target weight using the a greedy method.
  *
- * Greedy Efficient method: Use the heaviest to the lightest weight to calculate the list of plates needed.
+ * Lazy method: Use the heaviest to the lightest weight to calculate the list of plates needed.
+ * Note: Ignores the amount of plates there are and assumes the user has all the plates needed to get the required target weight.
+ * EX: List of availabe plates [45,35,25] sorted from greatest to least. 45lbs would be used then 35lb then 25lb etc...
  *
- * EX: List of availbe plates [45,35,25] sorted from greatest to least. 45lbs would be used then 35lb then 25lb etc...
- *
- * Note: By default will calculate for a conversion of pounds.
- *
- * @param targetWeight the total weight to add up to.
- * @param barbellWeight the weight of the barbell.
- * @param availablePlates the list of available plates to calculate the target weight for.
+ * @param availablePlates array of available
+ * @param sideTarget the target weight on each side of the babell. (e.g. barbell weight = 45, target weight = 135 => targetWeight - barbellWeight  => 90 / 2 => 45 is the sideTarget)
  * @return the list of plates needed to add to the targetWeight and left over target weight. (e.g. {plate? : Array<Plate>, leftoverWeight : number})
  */
-const getRequiredPlates = (
-  targetWeight: number,
-  barbellWeight: number,
+const calculateStandard = (
+  sideTarget: number,
   availablePlates: Array<Plate>
 ): { plates?: Array<Plate>; leftoverWeight: number } => {
-  // Target weight must be a value > 0 or else return default {undefined, -1} object.
-  if (targetWeight <= 0) {
-    return { plates: undefined, leftoverWeight: -1 };
-  }
+  const requiredPlates = new Array<Plate>();
   // Since we're using pop() for Array, the array will be sorted from least to greatest. [25,35,45] <== 45lb will be popped and removed.
   availablePlates = availablePlates.sort(
     (a: Plate, b: Plate) => a.type - b.type
   );
-
-  console.log(availablePlates);
-  console.log(barbellWeight);
-
-  const platesResult = Array<Plate>();
-
-  // Calculate the weight for one side of the barbell.
-  // EX: 135lbs - 45lbs = 90 / 2 => 45lbs per side.
-  let target = (targetWeight - barbellWeight) / 2;
-
-  while (target != 0 && availablePlates.length) {
+  while (sideTarget != 0 && availablePlates.length) {
     const currHeaviestPlate: Plate =
       availablePlates[availablePlates.length - 1];
-    if (currHeaviestPlate.type <= target) {
-      const amountNeeded = Math.trunc(target / currHeaviestPlate.type);
+    if (currHeaviestPlate.type <= sideTarget) {
+      const amountNeeded = Math.trunc(sideTarget / currHeaviestPlate.type);
       if (amountNeeded != 0) {
-        platesResult.push(
+        requiredPlates.push(
           new StandardPlate(currHeaviestPlate.type, amountNeeded)
         );
       }
-      target = target % currHeaviestPlate.type;
+      sideTarget = sideTarget % currHeaviestPlate.type;
     } else {
       availablePlates.pop();
     }
   }
-  return { plates: platesResult, leftoverWeight: target };
+  return { plates: requiredPlates, leftoverWeight: sideTarget };
 };
 
-const getRequiredPlatesLimited = {};
+/**
+ * Uses the same lazy method as @see calculateStandard but limits the calculation by how
+ * many plates are actually available in the Plate.amount property.
+ *
+ * @param availablePlates array of available
+ * @param sideTarget the target weight on each side of the babell. (e.g. barbell weight = 45, target weight = 135 => targetWeight - barbellWeight  => 90 / 2 => 45 is the sideTarget)
+ * @return the list of plates needed to add to the targetWeight and left over target weight. (e.g. {plate? : Array<Plate>, leftoverWeight : number})
+ */
+const calculateLimited = (
+  sideTarget: number,
+  availablePlates: Array<Plate>
+): { plates?: Array<Plate>; leftoverWeight: number } => {
+  const requiredPlates = new Array<Plate>();
+  // Since we're using pop() for Array, the array will be sorted from least to greatest. [25,35,45] <== 45lb will be popped and removed.
+  availablePlates = availablePlates.sort(
+    (a: Plate, b: Plate) => a.type - b.type
+  );
+  // Get the required plates needed with plate amount limitations in mind.
+  while (sideTarget > 0 && availablePlates.length) {
+    const currentPlate = availablePlates[availablePlates.length - 1];
+    const amountNeeded = Math.trunc(sideTarget / currentPlate.type);
+    const newPlate = new StandardPlate(currentPlate.type, 0);
+    if (amountNeeded > 0 && currentPlate.amount != 0) {
+      if (amountNeeded <= currentPlate.amount) {
+        newPlate.amount = amountNeeded;
+      } else {
+        newPlate.amount = currentPlate.amount;
+      }
+      requiredPlates.push(newPlate);
+      // Calculate remaining side target weight.
+      sideTarget = Math.abs(sideTarget - newPlate.type * newPlate.amount);
+    }
+    availablePlates.pop();
+  }
+  return { plates: requiredPlates, leftoverWeight: sideTarget };
+};
 
 /**
  * Calculate the required plates needed for the target weight, this function accepts a plate
@@ -208,19 +226,25 @@ const calcRequiredPlates = (
   targetWeight: number,
   config: PlateConfig
 ): { plates?: Array<Plate>; leftoverWeight: number } => {
-  const {
-    barbellWeight,
-    standardPlates: availablePlates,
-    selectedPlates,
-  } = config;
-  let userSelectedPlates = availablePlates;
+  // Target weight must be a value > 0 or else return default {undefined, -1} object.
+  if (targetWeight <= 0) {
+    return { plates: undefined, leftoverWeight: -1 };
+  }
+  const { barbellWeight, standardPlates, selectedPlates } = config;
+  let userSelectedPlates = standardPlates;
+  // Calculate the amount of weight per side needed on the barbell.
+  const sideTarget = Math.abs(targetWeight - barbellWeight) / 2;
+
   // Check if configuration is set to use the amount of available plates.
   if (!config.useLimitedPlates) {
     userSelectedPlates = userSelectedPlates.filter((plate) =>
       selectedPlates.has(plate.type)
     );
+    return calculateStandard(sideTarget, userSelectedPlates);
+  } else {
+    userSelectedPlates = userSelectedPlates.filter((plate) => plate.amount > 0);
+    return calculateLimited(sideTarget, userSelectedPlates);
   }
-  return getRequiredPlates(targetWeight, barbellWeight, userSelectedPlates);
 };
 
 export default {
